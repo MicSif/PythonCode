dd09a6cbd07e4352b4edc2b5a6af15e2eebdf36f
micsif <las1993810@126.com>
2017-12-19 11:09:03 +0800

    update

--- week5/cache_2.py
+++ week5/cache_2.py
@@ -1,20 +1,23 @@
 import json
+from functools import wraps
 
 class RedisCache:
     def __init__(self,redis_client):
         self._redis = redis_client
     def cache(self,timeout=0):
         def outer(func):
-            def inner():
+            @wraps(func)
+            def inner(*args,**kwargs):
                 if timeout==0:
-                    return func()
+                    return func(*args,**kwargs)
                 key=func.__name__
+                print(key)
                 value = self._redis.get(key)
                 if value:
                     return json.loads(value.decode('utf-8'))
                 else:
-                    msg=func()
-                    self._redis.setex(key,json.dumps(msg),timeout)
+                    msg=func(*args,**kwargs)
+                    self._redis.setex(key,timeout,json.dumps(msg))
                     return msg
             return inner
         return outer
